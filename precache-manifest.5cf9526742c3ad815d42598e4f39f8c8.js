2592e38ccfa9311d2e9c6868f9c79587161f1650
Shrikant Kale <milind_d@gmail.com>
2020-05-26 13:23:25 +0530

    Updates

---
+++ precache-manifest.5cf9526742c3ad815d42598e4f39f8c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88254e68be2ec8588ca659548b17058c",
+    "url": "/covid-dashboard-react/index.html"
+  },
+  {
+    "revision": "846b148a3f0d2fd8698a",
+    "url": "/covid-dashboard-react/static/css/main.57847559.chunk.css"
+  },
+  {
+    "revision": "82ad482a961aaca2fe26",
+    "url": "/covid-dashboard-react/static/js/2.05675aff.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-dashboard-react/static/js/2.05675aff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "846b148a3f0d2fd8698a",
+    "url": "/covid-dashboard-react/static/js/main.fa76cec4.chunk.js"
+  },
+  {
+    "revision": "4d149243e0033f953f23",
+    "url": "/covid-dashboard-react/static/js/runtime-main.ecc3365a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-dashboard-react/static/media/logo.d7265326.png"
+  }
+]);
